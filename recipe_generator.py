c3faecdf1ab8703005cc0e8322fd7faffd31a944
Brittany Wright <bmariew@brittanys-mbp-2.lan>
2020-10-11 16:51:03 -0400

    Added logic to collect a new recipe from user and add it to dataframe.

--- recipe_generator.py
+++ recipe_generator.py
@@ -1,16 +1,49 @@
 import pandas as pd
 
-df = pd.read_csv('recipes.csv', index_col=[0])
-#df = pd.DataFrame(columns=['Recipe', 'Ingredients', 'Instructions'])
 
-taco = {'Recipe': 'tacos2', 'Ingredients': ['beef', 'tortilla shells'], 'Instructions': [
-    '1. Brown beef', '2. Add taco seasoning']}
-df = df.append(taco, ignore_index=True)
-pasta = {'Recipe': 'pasta2', 'Ingredients': ['1 jar of pasta sauce', '1 box of noodles'], 'Instructions': [
-    '1. boil water', '2. boil nooods for 10 minutes']}
-df = df.append(pasta, ignore_index=True)
-waffles = {'Recipe': 'waffles2', 'Ingredients': [
-    'waffles', 'syrup'], 'Instructions': ['1. Toast waffle', '2. Pour syrup']}
-df = df.append(waffles, ignore_index=True)
-print(df.head())
+def initial_input():
+    response = input(
+        '''
+        Enter "New" if you would like to record a new recipe.
+        Enter "Random" if you would like a randomly generated recipe.
+
+        ''')
+    return response.lower()
+
+
+def add_recipe():
+    name = input(
+        '''
+        Great! What is the name of the recipe you would like to add?
+
+        ''')
+    ingred = input(
+        '''
+        Please provide a comma separated list of ingredients needed to make {}.
+
+        '''.format(name))
+    ingred = ingred.split(',')
+
+    instruct = input(
+        '''
+        Please provide a comma separated list of instructions need to make {}.
+
+        '''.format(name))
+    instruct = instruct.split(',')
+
+    row = {'Recipe': name, 'Ingredients': ingred, 'Instructions': instruct}
+    return row
+
+
+df = pd.read_csv('recipes.csv', index_col=[0])
+
+action = initial_input()
+if action == 'new':
+    new_row = add_recipe()
+    df = df.append(new_row, ignore_index=True)
+elif action == 'random':
+    print('Great! Randomly generated recipe coming up!')
+else:
+    print('Sorry, that response is invalid')
+
 df.to_csv('recipes.csv')
